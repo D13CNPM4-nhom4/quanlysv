8496149f5547352926c46041a2e723665eee338a
Your Name <you@example.com>
2021-05-03 21:24:40 +0700

    create form-register

---
+++ QLSV/Dangky.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLSV
+{
+    public partial class Dangky : Form
+    {
+        public Dangky()
+        {
+            InitializeComponent();
+        }
+
+        private void btndangky_Click(object sender, EventArgs e)
+        {
+            String conn = @"Data Source=ADMIN-2N12AHLMA\SQLEXPRESS;Initial Catalog=QLSV2;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conn);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into CanBo(Username,Password,Hoten) values('" + txt_tk.Text + "','" + txt_mk.Text + "','"+txthoten.Text+"')", con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Đăng ký thành công");
+            con.Close();
+        }
+
+        private void btnquay_Click(object sender, EventArgs e)
+        {
+            DangNhap form1 = new DangNhap();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
